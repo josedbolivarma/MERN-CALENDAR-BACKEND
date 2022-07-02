d49b7f251add1a58c3aa3d456e0c5c9b17d89bc5
Jose David <josedbolivarmafrontend@gmail.com>
2022-07-02 14:06:18 -0500

    Controllers

--- controlles/auth.js
+++ controlles/auth.js
@@ -93,7 +93,8 @@ const revalidarToken = async (req, res = response ) => {
 
         res.json({
             ok: true,
-            msg: 'renew',
+            uid,
+            name,
             token
         })
 }
